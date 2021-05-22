9a0b45e3e51e6ee935baca87adda535c7a1c13c9
gbdennisraith <gustavpezka@gmail.com>
2021-05-23 00:49:42 +0200

    homework for lesson 03, rework

--- src/main/java/lessons/lesson3_homework.java
+++ src/main/java/lessons/lesson3_homework.java
@@ -5,79 +5,72 @@ import java.util.Arrays;
 public class lesson3_homework {
     public static void main(String[] args) {
 /////Задание 1
-        int[] zerostoheros = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        zerosToHeroes(zerostoheros);
-        for (int i = 0; i < zerostoheros.length; i++) {
-            System.out.println(zerostoheros[i]);
-        }
+        int[] arrayZerosToHeroes = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        zerosToHeroes(arrayZerosToHeroes);
+        System.out.println(Arrays.toString(arrayZerosToHeroes));
+
 /////Задание 2
-        int[] tohundred = new int[100];
-        toHundred(tohundred);
-        for (int i = 0; i < tohundred.length; i++) {
-            System.out.println(tohundred[i]);
-        }
+        int[] arrayToHundred = new int[100];
+        toHundred(arrayToHundred);
+        System.out.println(Arrays.toString(arrayToHundred));
 /////Задание 3
-        int[] doublelessthansix = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        doubleLessThanSix(doublelessthansix);
-        for (int i = 0; i < doublelessthansix.length; i++) {
-            System.out.println(doublelessthansix[i]);
-        }
+        int[] arrayDoubleLessThanSix = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        doubleLessThanSix(arrayDoubleLessThanSix);
+        System.out.println(Arrays.toString(arrayToHundred));
 /////Задание 4
-        int[][] filldiagonals = new int[6][6];
-        fillDiagonals(filldiagonals);
-        for (int i = 0; i < filldiagonals.length; i++) {
-            for (int j = 0; j < filldiagonals[i].length; j++) {
-                System.out.print(filldiagonals[i][j]);
+        int[][] arrayFillDiagonals = new int[6][6];
+        fillDiagonals(arrayFillDiagonals);
+        for (int i = 0; i < arrayFillDiagonals.length; i++) {
+            for (int j = 0; j < arrayFillDiagonals[i].length; j++) {
+                System.out.print(arrayFillDiagonals[i][j]);
             }
             System.out.println();
         }
 /////Задание 5
         int len = 10;
         int initialValue = 2;
-        for (int i = 0; i < len; i++) {
-            System.out.println(returnArray(len,initialValue)[i]);
-        }
+        System.out.println(Arrays.toString(returnArray(len,initialValue)));
 /////Задание 6
         int[] minmax = {2,5,17,23,86,23,55,3,1,4,67,93};
         minMax(minmax);
 /////Задание 7
-        int[] checkbalance = {1,1,1,2,1,2,2};
+        int[] checkbalance = {1,1,1,2,1,2,2,10};
         System.out.println(checkBalance(checkbalance));
 /////Задание 8
         int[] shift = {1,2,3,5,7,8,9,3,4};
         shiftArray(shift,5);
     }
 
-    public static void zerosToHeroes (int[] zerostoheros){
-        for (int i = 0; i < zerostoheros.length; i++) {
-            if (zerostoheros[i] == 0){
-                zerostoheros[i] = 1;
-            }else if (zerostoheros[i] == 1){
-                zerostoheros[i] = 0;
+    public static void zerosToHeroes (int[] arrayZerosToHeroes){
+        for (int i = 0; i < arrayZerosToHeroes.length; i++) {
+            if (arrayZerosToHeroes[i] == 0){
+                arrayZerosToHeroes[i] = 1;
+            }else if (arrayZerosToHeroes[i] == 1){
+                arrayZerosToHeroes[i] = 0;
             }
         }
     }
 
-    public static void toHundred (int[] tohundred){
+    public static void toHundred (int[] arrayToHundred){
         for (int i = 0; i < 100; i++) {
-            tohundred[i] = i+1;
+            arrayToHundred[i] = i+1;
         }
     }
 
-    public static void doubleLessThanSix (int[] doublelessthansix){
-        for (int i = 0; i < doublelessthansix.length; i++) {
-            if (doublelessthansix[i] < 6){
-                doublelessthansix[i] = doublelessthansix[i]*2;
+    public static void doubleLessThanSix (int[] arrayDoubleLessThanSix){
+        for (int i = 0; i < arrayDoubleLessThanSix.length; i++) {
+            if (arrayDoubleLessThanSix[i] < 6){
+                arrayDoubleLessThanSix[i] = arrayDoubleLessThanSix[i]*2;
             }
         }
     }
 
-    public static void fillDiagonals (int[][] filldiagonals){
-        for (int i = 0; i < filldiagonals.length; i++) {
-            filldiagonals[i][i] = 1;
-            for (int j = 0; j < filldiagonals[i].length; j++) {
+    public static void fillDiagonals (int[][] arrayFillDiagonals){
+        for (int i = 0; i < arrayFillDiagonals.length; i++) {
+            arrayFillDiagonals[i][i] = 1;
+            for (int j = 0; j < arrayFillDiagonals[i].length; j++) {
                 if (i+j == 5){
-                    filldiagonals[i][j] = 1;
+                    arrayFillDiagonals[i][arrayFillDiagonals.length - 1 - i] = 1;
                 }
             }
         }
@@ -85,38 +78,31 @@ public class lesson3_homework {
 
     public static int[] returnArray (int len, int initialValue){
         int[] array = new int[len];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = initialValue;
-        }
+        Arrays.fill(array,initialValue);
         return array;
     }
 
-    public static void minMax(int[] minmax){
-        int min = minmax[0];
-        int max = minmax[0];
-        for (int i = 0; i < minmax.length; i++) {
-            if (minmax[i] > max){
-                max = minmax[i];
+    public static void minMax(int[] arrayMinMax){
+        int min = arrayMinMax[0];
+        int max = arrayMinMax[0];
+        for (int i = 0; i < arrayMinMax.length; i++) {
+            if (arrayMinMax[i] > max){
+                max = arrayMinMax[i];
             }
-            if (minmax[i] < min){
-                min = minmax[i];
+            if (arrayMinMax[i] < min){
+                min = arrayMinMax[i];
             }
         }
         System.out.println("minimum - " + min + " ; maximum - " + max);
     }
 
     public static boolean checkBalance(int[] checkbalance){
-        int sum = 0;
-        for (int i = 0; i < checkbalance.length; i++) {
-            sum += checkbalance[i];
-        }
-        if (sum%2 != 0) {
-            return false;
-        }
         int newsum = 0;
+        int sum = Arrays.stream(checkbalance).sum();
         for (int i = 0; i < checkbalance.length; i++) {
             newsum += checkbalance[i];
-            if(newsum == sum/2) {
+            System.out.println(newsum);
+            if(newsum == sum/2 && sum%2 == 0) {
                 return true;
             }
         }
@@ -141,8 +127,6 @@ public class lesson3_homework {
                 array[0]=last;
             }
         }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+        System.out.println(Arrays.toString(array));
     }
 }
